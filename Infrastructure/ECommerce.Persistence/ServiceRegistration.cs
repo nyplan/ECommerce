7ca066b13994eae1cf0dd426a94768ab183c418b
Nurlan Khankishiyev <nurlan.xankisiyev17@gmail.com>
2023-04-18 22:42:43 +0400

    Product, Order, Customer added

---
+++ Infrastructure/ECommerce.Persistence/ServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using ECommerce.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<ECommerceDbContext>(options =>
+            {
+                options.UseNpgsql(Configuration.ConnectionString);
+            });
+        }
+    }
+}
