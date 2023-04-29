61e95b90d2163d2261c6dca0d5f90c7066478d71
Nurlan Khankishiyev <nurlan.xankisiyev17@gmail.com>
2023-04-29 22:32:29 +0400

    Added CORS Policy

--- Presentation/ECommerce.API/Program.cs
+++ Presentation/ECommerce.API/Program.cs
@@ -2,12 +2,15 @@ using ECommerce.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddPersistenceServices();
-
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:4200", "http://localhost:4200").AllowAnyHeader().AllowAnyMethod();
+    });
+});
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -19,6 +22,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
