7371337376f84ec7d00d736b7a62fa8732d0cc56
Nurlan Khankishiyev <nurlan.xankisiyev17@gmail.com>
2023-05-02 22:49:37 +0400

    Added Fluent Validation

---
+++ Core/ECommerce.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,26 @@
+﻿using ECommerce.Application.ViewModels.Products;
+using FluentValidation;
+
+namespace ECommerce.Application.Validators.Products
+{
+    public class CreateProductValidator : AbstractValidator<CreateProduct>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(c => c.Name)
+            .NotEmpty()
+            .MinimumLength(3)
+            .MaximumLength(50);
+
+            RuleFor(c => c.Price)
+                .NotNull()
+                .InclusiveBetween(0.1, 10000)
+                .WithMessage("Price must be between 0.1 and 10000");
+
+            RuleFor(c => c.Stock)
+                .NotNull()
+                .InclusiveBetween(0, 1000)
+                .WithMessage("Stock must be between 0 and 1000");
+        }
+    }
+}
--- Presentation/ECommerce.API/Program.cs
+++ Presentation/ECommerce.API/Program.cs
@@ -1,4 +1,9 @@
+using ECommerce.Application.Validators.Products;
+using ECommerce.Infrastructure.Filters;
 using ECommerce.Persistence;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +15,21 @@ builder.Services.AddCors(options =>
         policy.WithOrigins("http://localhost:4200", "https://localhost:4200").AllowAnyHeader().AllowAnyMethod();
     });
 });
-builder.Services.AddControllers();
+builder.Services.AddControllers(c => c.Filters.Add<ValidationFilter>());
+//    .AddFluentValidation(conf => 
+//    conf.RegisterValidatorsFromAssemblyContaining<CreateProductValidator>())
+//    .ConfigureApiBehaviorOptions(options => 
+//    options.SuppressModelStateInvalidFilter = true);
+
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddFluentValidationClientsideAdapters();
+builder.Services.AddValidatorsFromAssemblyContaining<CreateProductValidator>();
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
